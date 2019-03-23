6bdb1505da460271e8bf4492cb79ccf57013f3c5
Alexey <zolotukhin89@gmail.com>
2019-03-23 16:51:50 +0300

    ALLLOWED_HOSTS

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pb9_=wl8h-ogket4kzk+bc)^bxc6zl4vrlfb4coldsoys^pij)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.168.18.82', ]
 
 
 # Application definition

ab5ce9092ada94a29d6f34a7aa9362fd12b73628
Alexey <zolotukhin89@gmail.com>
2019-03-23 17:53:00 +0300

    Fix

--- biocad/management/commands/import_products.py
+++ biocad/management/commands/import_products.py
@@ -12,9 +12,9 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options ):
         data_dir = os.path.join(os.path.dirname(__file__), '../../../data')
-        file_path = os.path.join(os.path.normpath(data_dir), 'product.csv')
+        file_path = os.path.join(os.path.normpath(data_dir), 'product.tsv')
         with open(file_path) as csvfile:
-            rows = csv.reader(csvfile)
+            rows = csv.reader(csvfile, delimiter='\t')
             next(rows, None)
             for row in rows:
                 product_id = row[0]
